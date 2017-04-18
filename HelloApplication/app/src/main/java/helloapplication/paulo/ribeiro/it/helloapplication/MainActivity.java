565fe6a5cfa6b1da29c5d9ab56534a61475c4763
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-04-18 20:55:45 -0300

    Criação do projeto

---
+++ HelloApplication/app/src/main/java/helloapplication/paulo/ribeiro/it/helloapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package helloapplication.paulo.ribeiro.it.helloapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "livro";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TAG,"log de verbose");
+        Log.d(TAG,"log de debug");
+        Log.i(TAG,"log de info");
+        Log.w(TAG,"log de alerta");
+        //Log.e(TAG,"log de erro", new RuntimeException("teste de erro"));
+    }
+}
