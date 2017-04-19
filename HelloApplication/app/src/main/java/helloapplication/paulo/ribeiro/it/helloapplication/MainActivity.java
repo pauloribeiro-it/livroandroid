0baf5c8360d33b9ac48cc80ef06cd8e5c3d76f68
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-04-18 22:03:03 -0300

    Login e senha

--- HelloApplication/app/src/main/java/helloapplication/paulo/ribeiro/it/helloapplication/MainActivity.java
+++ HelloApplication/app/src/main/java/helloapplication/paulo/ribeiro/it/helloapplication/MainActivity.java
@@ -2,18 +2,33 @@ package helloapplication.paulo.ribeiro.it.helloapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "livro";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v(TAG,"log de verbose");
-        Log.d(TAG,"log de debug");
-        Log.i(TAG,"log de info");
-        Log.w(TAG,"log de alerta");
-        //Log.e(TAG,"log de erro", new RuntimeException("teste de erro"));
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(this);
+    }
+    private void alert(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+    public void onClick(View v) {
+        TextView tLogin = (TextView) findViewById(R.id.tLogin);
+        TextView tSenha = (TextView) findViewById(R.id.tSenha);
+        String login = tLogin.getText().toString();
+        String senha = tSenha.getText().toString();
+
+        if ("paulo".equals(login) && "123".equals(senha)) {
+            alert("Bem-vindo, login realizado com sucesso.");
+        } else {
+            alert("Login e senha incorretos.");
+        }
     }
 }
