6981621c28b72115339ce05a7ebfbe68c0bdf5a2
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-04-18 22:07:44 -0300

    Refactoring

--- HelloApplication/app/src/main/java/helloapplication/paulo/ribeiro/it/helloapplication/MainActivity.java
+++ HelloApplication/app/src/main/java/helloapplication/paulo/ribeiro/it/helloapplication/MainActivity.java
@@ -7,28 +7,34 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btLogin = (Button) findViewById(R.id.btLogin);
-        btLogin.setOnClickListener(this);
+        btLogin.setOnClickListener(onClickLogin());
     }
     private void alert(String msg){
         Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
-    public void onClick(View v) {
-        TextView tLogin = (TextView) findViewById(R.id.tLogin);
-        TextView tSenha = (TextView) findViewById(R.id.tSenha);
-        String login = tLogin.getText().toString();
-        String senha = tSenha.getText().toString();
 
-        if ("paulo".equals(login) && "123".equals(senha)) {
-            alert("Bem-vindo, login realizado com sucesso.");
-        } else {
-            alert("Login e senha incorretos.");
-        }
+
+    private View.OnClickListener onClickLogin(){
+        return new Button.OnClickListener(){
+            public void onClick(View v) {
+                TextView tLogin = (TextView) findViewById(R.id.tLogin);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+
+                if ("paulo".equals(login) && "123".equals(senha)) {
+                    alert("Bem-vindo, login realizado com sucesso.");
+                } else {
+                    alert("Login e senha incorretos.");
+                }
+            }
+        };
     }
 }
