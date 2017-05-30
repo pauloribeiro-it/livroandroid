b65dda21940c60aac260ac85e5ff47d1080782a7
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-05-29 21:50:46 -0300

    Splash Screen

---
+++ HelloHandler/app/src/main/java/hellofragments/paulo/ribeiro/it/hellohandler/SplashScreenAtivity.java
@@ -0,0 +1,22 @@
+package hellofragments.paulo.ribeiro.it.hellohandler;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenAtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(),MainActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
