4fda64ba87fe2e7af2dc08683548f54e49db1eac
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-07-01 10:38:55 -0300

    Swipe to refresh

--- ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/fragments/SiteLivroFragment.java
+++ ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/fragments/SiteLivroFragment.java
@@ -2,6 +2,7 @@ package hellomaterial.paulo.ribeiro.it.fragments;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +11,14 @@ import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import hellomaterial.paulo.ribeiro.it.R;
 
+import static hellomaterial.paulo.ribeiro.it.R.color.refresh_progress_1;
+
 public class SiteLivroFragment extends livroandroid.lib.fragment.BaseFragment {
 
     private static final String URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm";
     private WebView webView;
     private ProgressBar progress;
+    protected SwipeRefreshLayout swipeLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,7 +28,19 @@ public class SiteLivroFragment extends livroandroid.lib.fragment.BaseFragment {
         progress = (ProgressBar) view.findViewById(R.id.progress);
         setWebViewClient(webView);
         webView.loadUrl(URL_SOBRE);
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeToRefresh);
+        swipeLayout.setOnRefreshListener(OnRefreshListener());
+        swipeLayout.setColorSchemeColors(refresh_progress_1,R.color.refresh_progress_2,R.color.refresh_progress_3);
         return view;
+    }
+
+    private SwipeRefreshLayout.OnRefreshListener OnRefreshListener(){
+        return new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        };
     }
 
     private void setWebViewClient(WebView webview){
@@ -38,6 +54,7 @@ public class SiteLivroFragment extends livroandroid.lib.fragment.BaseFragment {
             @Override
             public void onPageFinished(WebView view, String url) {
                 progress.setVisibility(View.INVISIBLE);
+                swipeLayout.setRefreshing(false);
             }
         });
     }
