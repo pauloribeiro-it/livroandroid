3a070c12a7820fbfd890612e7fcbbed5734a5fd4
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-07-08 09:28:46 -0300

    Corrigindo fragments

--- ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/activity/CarroActivity.java
+++ ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/activity/CarroActivity.java
@@ -1,9 +1,11 @@
 package hellomaterial.paulo.ribeiro.it.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import hellomaterial.paulo.ribeiro.it.R;
 import hellomaterial.paulo.ribeiro.it.domain.Carro;
-import hellomaterial.paulo.ribeiro.it.fragments.CarrosFragment;
+import hellomaterial.paulo.ribeiro.it.fragments.CarroFragment;
 
 public class CarroActivity extends BaseActivity {
 
@@ -11,14 +13,12 @@ public class CarroActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carro);
-
         setUpToolbar();
-
         Carro c = getIntent().getParcelableExtra("carro");
         getSupportActionBar().setTitle(c.nome);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        if(savedInstanceState != null){
-            CarrosFragment frag = new CarrosFragment();
+        if(savedInstanceState == null){
+            CarroFragment frag = new CarroFragment();
             frag.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction().add(R.id.CarroFragment,frag).commit();
         }
