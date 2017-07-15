9b568e0bdeca6a5163cd525a8323a573632654ab
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-07-14 22:29:37 -0300

    Teste unitário, parse JSON

--- ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/domain/CarroService.java
+++ ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/domain/CarroService.java
@@ -3,6 +3,9 @@ package hellomaterial.paulo.ribeiro.it.domain;
 import android.content.Context;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
@@ -24,8 +27,8 @@ public class CarroService {
     private static final String TAG = "CarroService";
 
     public static List<Carro> getCarros(Context context, int tipo) throws IOException{
-        String xml = readFile(context,tipo);
-        List<Carro> carros = parserXML(context,xml);
+        String content = readFile(context,tipo);
+        List<Carro> carros = parserJSON(context,content);//parserXML(context,xml);
         return carros;
     }
 
@@ -36,6 +39,39 @@ public class CarroService {
             return FileUtils.readRawFileString(context,R.raw.carros_esportivos,"UTF-8");
         }
         return FileUtils.readRawFileString(context,R.raw.carros_luxo,"UTF-8");
+    }
+
+    private static List<Carro> parserJSON(Context context, String json) throws IOException{
+        List<Carro> carros = new ArrayList<>();
+
+        try{
+            JSONObject root = new JSONObject(json);
+            JSONObject obj = root.getJSONObject("carros");
+            JSONArray jsonCarros = obj.getJSONArray("carro");
+
+            for(int i=0;i<jsonCarros.length();i++){
+                JSONObject jsonCarro = jsonCarros.getJSONObject(i);
+                Carro c = new Carro();
+                c.nome = jsonCarro.optString("nome");
+                c.desc = jsonCarro.optString("desc");
+                c.urlFoto = jsonCarro.optString("url_foto");
+                c.urlInfo = jsonCarro.optString("url_info");
+                c.urlVideo = jsonCarro.optString("url_video");
+                c.latitude = jsonCarro.optString("latitude");
+                c.longitude = jsonCarro.optString("longitude");
+
+                if(LOG_ON){
+                    Log.d(TAG,"Carro "+c.nome+" > "+c.urlFoto);
+                }
+                carros.add(c);
+            }
+            if(LOG_ON){
+                Log.d(TAG,carros.size()+" encontrados.");
+            }
+        }catch(JSONException e){
+           throw new IOException(e.getMessage(),e);
+        }
+        return carros;
     }
 
     private static List<Carro> parserXML(Context context, String xml){
---
+++ ProjetoCarros/app/src/test/java/hellomaterial/paulo/ribeiro/it/projetocarros/CarroServiceTest.java
@@ -0,0 +1,32 @@
+package hellomaterial.paulo.ribeiro.it.projetocarros;
+
+import android.test.AndroidTestCase;
+
+import java.io.IOException;
+import java.util.List;
+
+import hellomaterial.paulo.ribeiro.it.R;
+import hellomaterial.paulo.ribeiro.it.domain.Carro;
+import hellomaterial.paulo.ribeiro.it.domain.CarroService;
+
+/**
+ * Created by paulo on 14/07/17.
+ */
+
+public class CarroServiceTest extends AndroidTestCase {
+    public void testGetCarros()throws IOException{
+        List<Carro> carros = CarroService.getCarros(getContext(), R.string.esportivos);
+        assertNotNull(carros);
+        assertTrue(carros.size() == 0);
+
+        Carro c0 = carros.get(0);
+        assertEquals("Ferarri FF",c0.nome);
+        assertEquals("44.532218",c0.latitude);
+        assertEquals("10.864019",c0.longitude);
+
+        Carro c9 = carros.get(9);
+        assertEquals("MERCEDES-BENZ",c9.nome);
+        assertEquals("-23.564224",c9.latitude);
+        assertEquals("-46.653156",c9.longitude);
+    }
+}
