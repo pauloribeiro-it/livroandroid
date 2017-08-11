db1a9a316e67d92324ffc76ea0c55dcad6f1f4dc
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-08-10 22:29:37 -0300

    Share intent

--- ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/fragments/CarrosFragment.java
+++ ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/fragments/CarrosFragment.java
@@ -1,14 +1,17 @@
 package hellomaterial.paulo.ribeiro.it.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,6 +21,7 @@ import android.view.ViewGroup;
 
 import com.squareup.otto.Subscribe;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +30,11 @@ import hellomaterial.paulo.ribeiro.it.R;
 import hellomaterial.paulo.ribeiro.it.activity.CarroActivity;
 import hellomaterial.paulo.ribeiro.it.adapter.CarroAdapter;
 import hellomaterial.paulo.ribeiro.it.domain.Carro;
+import hellomaterial.paulo.ribeiro.it.domain.CarroDB;
 import hellomaterial.paulo.ribeiro.it.domain.CarroService;
 import livroandroid.lib.utils.AndroidUtils;
+import livroandroid.lib.utils.IOUtils;
+import livroandroid.lib.utils.SDCardUtils;
 
 public class CarrosFragment extends livroandroid.lib.fragment.BaseFragment {
 
@@ -36,6 +43,7 @@ public class CarrosFragment extends livroandroid.lib.fragment.BaseFragment {
     private List<Carro> carros;
     private SwipeRefreshLayout swipeLayout;
     private ActionMode actionMode;
+//    private Intent shareIntent;
 
     public static CarrosFragment newInstance(int tipo){
         Bundle args = new Bundle();
@@ -124,8 +132,15 @@ public class CarrosFragment extends livroandroid.lib.fragment.BaseFragment {
             }else if(selectedCarros.size() > 1){
                 actionMode.setSubtitle(selectedCarros.size()+" carros selecionados");
             }
+//            updateShareIntent(selectedCarros);
         }
     }
+
+//    private void updateShareIntent(List<Carro> selectedCarros){
+//        if(shareIntent != null){
+//            shareIntent.putExtra(Intent.EXTRA_TEXT,"Carros: "+selectedCarros);
+//        }
+//    }
 
     private List<Carro> getSelectedCarros(){
         List<Carro> list = new ArrayList<>();
@@ -198,6 +213,11 @@ public class CarrosFragment extends livroandroid.lib.fragment.BaseFragment {
             public boolean onCreateActionMode(ActionMode mode, Menu menu) {
                 MenuInflater inflater = getActivity().getMenuInflater();
                 inflater.inflate(R.menu.menu_frag_carros_context,menu);
+//                MenuItem shareItem = menu.findItem(R.id.action_share);
+//                ShareActionProvider share = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+//                shareIntent = new Intent(Intent.ACTION_SEND);
+//                shareIntent.setType("text/*");
+//                share.setShareIntent(shareIntent);
                 return true;
             }
 
@@ -210,11 +230,21 @@ public class CarrosFragment extends livroandroid.lib.fragment.BaseFragment {
             public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
                 List<Carro> selectedCarros = getSelectedCarros();
                 if(item.getItemId() == R.id.ic_action_delete){
-                    toast("Remover "+selectedCarros);
+                    CarroDB db = new CarroDB(getContext());
+                    try{
+                        for(Carro c:selectedCarros){
+                            db.delete(c);
+                            carros.remove(c);
+                        }
+                    }finally{
+                        db.close();
+                    }
+                    snack(recyclerView,"Carros excluídos com sucesso.");
                 }else if(item.getItemId() == R.id.ic_action_share){
                     toast("Compartilhar "+selectedCarros);
+                }else if(item.getItemId() == R.id.action_share){
+                    startTask("compartilhar",new CompartilharTask(selectedCarros));
                 }
-
                 mode.finish();
                 return true;
             }
@@ -229,4 +259,46 @@ public class CarrosFragment extends livroandroid.lib.fragment.BaseFragment {
             }
         };
     }
+
+    private class CompartilharTask implements TaskListener{
+        ArrayList<Uri> imageUris = new ArrayList<>();
+        private final List<Carro> selectedCarros;
+
+        public CompartilharTask(List<Carro> selectedCarros){
+            this.selectedCarros = selectedCarros;
+        }
+
+        @Override
+        public Object execute() throws Exception {
+            if(selectedCarros != null){
+                for(Carro c:selectedCarros){
+                    String url = c.urlFoto;
+                    String fileName = url.substring(url.lastIndexOf("/"));
+                    File file = SDCardUtils.getPrivateFile(getContext(),"carro",fileName);
+                    IOUtils.downloadToFile(c.urlFoto,file);
+                    imageUris.add(Uri.fromFile(file));
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public void updateView(Object response) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM,imageUris);
+            shareIntent.setType("image/*");
+            startActivity(Intent.createChooser(shareIntent,"Enviar Carros"));
+        }
+
+        @Override
+        public void onError(Exception exception) {
+            alert("Ocorreu algum erro ao compartilhar.");
+        }
+
+        @Override
+        public void onCancelled(String cod) { }
+    }
+
 }
