cb787b6918bb1cffe7ff2f4d9a87d63c5ebc0b5e
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-08-29 21:48:45 -0300

    Tipos de mapa

--- ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/fragments/MapaFragment.java
+++ ProjetoCarros/app/src/main/java/hellomaterial/paulo/ribeiro/it/fragments/MapaFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -29,6 +32,7 @@ public class MapaFragment extends BaseFragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mapa,container,false);
+        setHasOptionsMenu(true);
         SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
         this.carro = getArguments().getParcelable("carro");
@@ -46,5 +50,38 @@ public class MapaFragment extends BaseFragment implements OnMapReadyCallback {
             map.addMarker(new MarkerOptions().title(carro.nome).snippet(carro.desc).position(location));
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_frag_mapa,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(map != null && carro != null){
+            if(item.getItemId() == R.id.action_location_carro){
+                LatLng location = new LatLng(Double.parseDouble(carro.latitude),Double.parseDouble(carro.longitude));
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(location,13));
+            }else if(item.getItemId() == R.id.action_location_directions){
+                toast("Mostrar rota/direções até a fábrica.");
+            }else if(item.getItemId() == R.id.action_zoom_in){
+                toast("zoom +");
+                map.animateCamera(CameraUpdateFactory.zoomIn());
+            }else if(item.getItemId() == R.id.action_zoom_out){
+                toast("zoom -");
+                map.animateCamera(CameraUpdateFactory.zoomOut());
+            }else if(item.getItemId() == R.id.action_mapa_normal){
+                map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }else if(item.getItemId() == R.id.action_mapa_satelite){
+                map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }else if(item.getItemId() == R.id.action_mapa_terreno){
+                map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            }else if(item.getItemId() == R.id.action_mapa_hibrido){
+                map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
